c757be8bcc9891ddf4e5cc61b0555549873ec619
panyushkins <panyushkin@me.com>
2017-02-13 18:16:31 +0300

    Added if __name__ == '__main__':, made one more method

--- make-film-base.py
+++ make-film-base.py
@@ -20,29 +20,33 @@ def make_tmdb_api_request(method, api_key, extra_params=None):
     return load_json_data_from_url(url, params)
 
 
-def save_to_file(filename, save_file):
-    with open(filename, 'w') as file:
+def save_to_file(file_name, save_file):
+    with open(file_name, 'w') as file:
         file.write(save_file)
-    print('Data saved to ' + filename)
+    print('Data saved to ' + file_name)
 
-Films = dict()
 
-api_token = input('Input API key: ')
-filename = input('Input destination path to file: ')
-
-i = 1
-while len(Films) < 1000:
-    try:
-        Films[i] = make_tmdb_api_request(method='/movie/' + str(i), api_key=api_token)
-        getlists = make_tmdb_api_request(method='/movie/' + str(i) + '/lists', api_key=api_token)
-        Films[i].update(getlists)
-        i += 1
-
-    except urllib.error.HTTPError as e:
-        if e.code == 404:  # если 404 - пропускаем этот ID
+def download_films(token):
+    films = dict()
+    i = 1
+    while len(films) < 10:
+        try:
+            films[i] = make_tmdb_api_request(method='/movie/' + str(i), api_key=token)
+            get_lists = make_tmdb_api_request(method='/movie/' + str(i) + '/lists', api_key=token)
+            films[i].update(get_lists)
             i += 1
 
-print('Downloaded!')
-serialized_films = json.dumps(Films)
+        except urllib.error.HTTPError as e:
+            if e.code == 404:  # если 404 - пропускаем этот ID
+                i += 1
+    print('Downloaded!')
+    return films
 
-save_to_file(filename, serialized_films)
+
+if __name__ == '__main__':
+    api_token = input('Input API key: ')
+    filename = input('Input destination path to file: ')
+
+    Films_dict = download_films(api_token)
+    serialized_films = json.dumps(Films_dict)
+    save_to_file(filename, serialized_films)
