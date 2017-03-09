e3edc1114c263df275103701199019e59bae1258
panyushkins <panyushkin@me.com>
2017-03-09 16:51:53 +0300

    Renamed 'word' to 'keyword'

--- find-title.py
+++ find-title.py
@@ -3,11 +3,11 @@ import helpers
 
 def print_titles(films):
     for i in films:
-        if films[i]['title'].find(word) >= 0:
+        if films[i]['title'].find(keyword) >= 0:
             print(films[i]['title'])
 
 if __name__ == '__main__':
     path = input('Input path to file: ')
-    word = input('Input a word from film title: ')
+    keyword = input('Input a word from film title: ')
     films_dict = helpers.read_from_file(path)
     print_titles(films_dict)
